23168777c9408adff0edd3b988868a448b833d17
Ranime <156202462+Ranimev2@users.noreply.github.com>
2024-01-22 19:34:32 +0700

    Update +page.server.ts

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -5,11 +5,11 @@ export const load: Load = async () => {
 	const servers = serverLists();
 
 	const connection: { link: string; text: string; platform: string }[] = [
-		{ link: 'https://github.com/binsarjr', text: 'github.com/binsarjr', platform: 'Github' },
+		{ link: 'https://www.webtonative.com/checkout/nGDzhqqbZ1EgpgvDisrG', text: 'ranimev3', platform: 'Aplikasi' },
 		{
-			link: 'https://www.linkedin.com/in/binsarjr/',
-			text: 'Binsar Dwi Jasuma',
-			platform: 'LinkedIn'
+			link: 'https://myranime.my.id/',
+			text: 'riza',
+			platform: 'Profil'
 		}
 	];
 	return {
